02ee738b3fd5ece99318be77f6b81d1b1f22a480
Rafael Miranda <rafael.silva@leega.com.br>
2020-07-06 13:39:31 -0300

    First Commit

---
+++ src/test/java/com/workshop_mongodb/WorkshopmongodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.workshop_mongodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WorkshopmongodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
